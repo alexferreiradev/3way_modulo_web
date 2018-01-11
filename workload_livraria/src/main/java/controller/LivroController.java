0205df6155b1d69b45843c66e67f08aa210cf89c
Alex R. Ferreira <arf92@live.com>
2018-01-11 08:39:57 -0200

    adicao de teste com jsp atraves de atributo.
    Teste de isolamento entre jsp e camada de negocios.

--- workload_livraria/src/main/java/controller/LivroController.java
+++ workload_livraria/src/main/java/controller/LivroController.java
@@ -1,7 +1,10 @@
 package controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,14 +15,21 @@ public class LivroController extends HttpServlet {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
+	
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.sendRedirect("index.html");
+		
+		RequestDispatcher rd = req.getRequestDispatcher("/pages/teste.jsp");
+		List<String> lista = new ArrayList<String>();
+		lista.add("teste1");
+		lista.add("teste2");
+		lista.add("teste3");
+		lista.add("teste4");
+		lista.add("teste5");
+		lista.add("teste6");
+		req.setAttribute("at", lista);
+		rd.include(req, resp);
 	}
-
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.sendRedirect("index.html");
-	}
+	
 }
