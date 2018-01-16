e727821f49622bd7a63594f0aaa981dcbcdf8cac
javaterqui <javaterqui@suporte>
2018-01-16 20:30:35 -0200

    #4 msg: Refatoracao dao para usar exception

--- workload_livraria/src/main/java/service/LivroService.java
+++ workload_livraria/src/main/java/service/LivroService.java
@@ -1,5 +1,34 @@
 package service;
 
-public class LivroService {
+import java.util.ArrayList;
+import java.util.List;
+
+import data.dao.LivroDao;
+import data.dao.exception.DAOException;
+import data.model.Livro;
+import service.exception.ServiceException;
+
+public class LivroService implements LivroServiceInterface {
+
+	@Override
+	public List<Livro> listLivros() throws ServiceException {
+		List<Livro> livros = new ArrayList<>();
+		
+		try {
+			LivroDao livroDao = new LivroDao();
+			List<Livro> livrosPorPreco = livroDao.livrosPorPreco(0);
+			if (livrosPorPreco != null) {
+				livros = livrosPorPreco;
+			}
+		} catch (DAOException e) {
+			e.printStackTrace();
+			throw new ServiceException("Erro no banco ao listar livros: " + e.getMessage());
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new ServiceException("Erro desconhecido no banco ao listar livros.");
+		}
+		
+		return livros;
+	}
 
 }
