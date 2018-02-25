640a457969209856a95182cdb57fa791dad34e95
Alex Ferreira <arf92@live.com>
2018-02-25 11:05:36 -0300

    issue #53 msg: remocao de arquivos nao utilizados
    * remocao de classes de amostra de testes de junit

--- workload_livraria/src/main/java/data/Calc2.java
+++
@@ -1,9 +0,0 @@
-package data;
-
-public class Calc2 implements Calc2I{
-	
-	public String sum(int x1, int x2) {
-		return "";
-	}
-
-}
--- workload_livraria/src/main/java/data/Calc2I.java
+++
@@ -1,7 +0,0 @@
-package data;
-
-public interface Calc2I {
-	
-	String sum(int x1, int x2);
-
-}
